d32855ded3fda6ecfb3f7539f3174b8cdf084f23
Ismail Hossain <ismail301515@gmail.com>
2025-04-30 22:20:23 +0600

    first commit

---
+++ module1/src/1.4.ts
@@ -0,0 +1,38 @@
+//basic Data type
+
+//string
+let firstName = "Ismail"; //Implicit typing
+
+let lastName: string = "Hossain"; //Explicit Typing
+
+//number
+let roll: number = 14;
+//boolean
+let isAdmin: boolean = true;
+
+//undefined
+let x: undefined = undefined;
+
+//null
+let y: null = null;
+
+//any type
+let d;
+
+d = 123;
+d = "alsdkflks";
+d = true;
+d = null;
+
+//non-primitive
+
+//Array
+let friends: string[] = ["rachel", "monica"];
+
+//friends.push(1); //error
+
+let eligibleRollList: number[] = [1, 2, 3];
+
+//tuple --> one kind of array --> type of values
+let coordinates: [number, number] = [1, 5];
+let age: [number, string, boolean] = [50, "ismail", true];
---
+++ module1/src/1.5.ts
@@ -0,0 +1,15 @@
+//Reference type--> object
+
+const user: {
+  company: "Programming Hero"; //type -->literal types
+  firstName: string;
+  middleName?: string | undefined; //optional type
+  lastName: string;
+  isMarried: boolean;
+} = {
+  company: "Programming Hero",
+  firstName: "Ismail",
+  // middleName: "Hossain",
+  lastName: "Arafat",
+  isMarried: false,
+};
